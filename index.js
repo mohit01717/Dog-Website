98d06061842b0d66c8e72148e92251c64aabafff
mohit01717 <mohitgagneja943@gmail.com>
2024-01-18 15:04:43 +0530

    dog website

---
+++ index.js
@@ -0,0 +1,12 @@
+const car_d = document.querySelector(".card h1");
+const btn = document.querySelector(".btn");
+btn.addEventListener("click",()=>{
+      car_d.classList.toggle("click")
+})
+
+const li = document.querySelector("li a")
+console.log(li.attributes)
+console.log(li.getAttribute("href"))
+li.setAttribute("href","https://github.com")
+console.log(li.getAttribute("href"))
+
